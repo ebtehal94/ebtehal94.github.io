b5840d5410b7c862130899f70bb13052e6ad69d6
ebtehal94 <41169440+ebtehal94@users.noreply.github.com>
2020-09-21 14:19:37 +0300

    Add files via upload

---
+++ Taibah university/js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+    'use strict';
+    
+
+    //Scroll To Top
+    
+    $(window).scroll(function(){
+
+        if ($(this).scrollTop() >= 100) {
+
+             $ ('.scroll-to-top') .fadeIn(400);
+
+            } else {
+
+            $ ('.scroll-to-top') .fadeOut(400);
+                
+        }
+    });
+    
+    $('.scroll-to-top ').click(function() {
+        
+        $('html , body').animate({scrollTop : 0},800);
+        
+    });
+
+
+});
